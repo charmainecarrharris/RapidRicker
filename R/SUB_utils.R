016a3aab0d228465d1f7efd77f91521a6f27e6bb
SOLV-Code <30556192+SOLV-Code@users.noreply.github.com>
2020-11-18 12:06:39 -0800

    dependencies handling

---
+++ R/SUB_utils.R
@@ -0,0 +1,20 @@
+include.dependencies <- function(){
+#' @title Internal handling of dependencies
+#'
+#' @description Seems like this is needed in addition to the import list in the DESCRIPTION file, so keeping them all together in one place.
+#' @import dplyr
+#' @import tidyverse
+#' @import coda
+#' @import R2jags
+#' @import rstan
+#' @import rstanarm
+#' @return Nothing returned.
+#' @export
+
+
+
+print("including dependencies")
+
+
+} # end function include.dependencies
+
